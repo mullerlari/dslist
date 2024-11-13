ac86ab2d311c2fad7a756ac917364f54840b6eef
Larissa Muller <larissa.muller@outlook.com.br>
2024-11-13 20:36:40 -0300

    Game seed

--- src/main/java/com/mullerlari/dslist/entities/Game.java
+++ src/main/java/com/mullerlari/dslist/entities/Game.java
@@ -22,23 +22,29 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
-	private String imUrl;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String imUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
-		this.imUrl = imUrl;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -75,20 +81,28 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
 	}
 
-	public String getImUrl() {
-		return imUrl;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
-	public void setImUrl(String imUrl) {
-		this.imUrl = imUrl;
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
