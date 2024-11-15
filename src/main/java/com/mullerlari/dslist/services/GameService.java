e31d66f1120c5477ba939bfbd505d32d240a7860
Larissa Muller <larissa.muller@outlook.com.br>
2024-11-15 17:51:22 -0300

    Get game by Id

--- src/main/java/com/mullerlari/dslist/services/GameService.java
+++ src/main/java/com/mullerlari/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mullerlari.dslist.dto.GameDTO;
 import com.mullerlari.dslist.dto.GameMinDTO;
 import com.mullerlari.dslist.entities.Game;
 import com.mullerlari.dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();//busca no BD
+		return new GameDTO(result);//Converte para DTO
+		
+	}
+
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // transformando a GameList em DTO
